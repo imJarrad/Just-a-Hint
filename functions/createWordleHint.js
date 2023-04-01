825d8732d7e96761218aabffeaaa2bd660b7e152
ImJarrad <jarradmclean@gmail.com>
2023-04-01 11:07:16 -0600

    restructuing netlify function

--- functions/createWordleHint.js
+++ functions/createWordleHint.js
@@ -8,103 +8,120 @@ const openaiConfig = new Configuration({ apiKey: openaiApiKey });
 const openai = new OpenAIApi(openaiConfig);
 
 
-exports.handler = async (event, context) => {
-    try {
-      // Replace with your GitHub Personal Access Token and OpenAI API Key
-      const accessToken = process.env.GITHUB_PERSONAL_ACCESS_TOKEN;
-      const openaiApiKey = process.env.OPENAI_API_KEY;
-  
-      // Replace with your repo's owner and name
-      const repoOwner = "imJarrad";
-      const repoName = "Just-a-Hint";
-  
-      // Get the daily Wordle answer (using the correct Wordle API)
-      // Format the date to use in the API URL
-      const date = new Date();
-      const formattedDate = date.toISOString().slice(0, 10);
-  
-      // This makes a request to the Wordle API to get the daily answer
-      const wordleResponse = await axios.get(`https://www.nytimes.com/svc/wordle/v2/${formattedDate}.json`);
-      const wordleAnswer = wordleResponse.data.solution;
-  
-      // Get a hint from ChatGPT for the Wordle answer
-      // This function will be defined to call the ChatGPT API with the Wordle answer
-      const hint = await getHintFromChatGPT(wordleAnswer);
-  
-      // Create the contents of the .mdx file
-      // Generate the file name based on today's date and the .mdx extension
-        const month = date.toLocaleString('default', { month: 'long' });
-        const day = date.getDate();
-        const fileName = `Wordle hint - ${month} ${day}.mdx`;
 
-  
-      // Assemble the file contents using the hint and the date
-      const fileContents = 
-        `---
-        title: Wordle Hint for ${formattedDate}
-        date: ${formattedDate}
-        ---
-        # Wordle Hint for ${formattedDate}
-        Here's a hint to help you solve today's Wordle: **${hint}**
-        `;
+// This function will call the Wordle API to get the answer for the given date
+async function fetchWordle(date) {
+  try {
+    const response = await axios.get(`https://www.nytimes.com/svc/wordle/v2/${date.toISOString().slice(0, 10)}.json`);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching Wordle data:", error);
+    throw error;
+  }
+}
 
-    // Initialize the Octokit library with your GitHub Personal Access Token
+// This function will call the OpenAI ChatGPT API to write a hint for the given word
+async function getHintFromChatGPT(word) {
+  try {
+    const prompt = `Write a hint for the Wordle word: ${word}`;
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: prompt,
+      max_tokens: 50,
+      n: 1,
+      stop: null,
+      temperature: 0.7,
+    });
+
+    if (response.data.choices && response.data.choices.length > 0) {
+      return response.data.choices[0].text.trim();
+    } else {
+      console.error("Error: No choices found in the API response:", response);
+      console.log("Choices array content:", response.data.choices); // Log the content of the choices array
+      throw new Error("No choices found in the API response");
+    }
+  } catch (error) {
+    console.error("Error calling OpenAI API:", error);
+    throw error;
+  }
+}
+
+// This function will assemble the content for the new hint file
+function createHintFileContent(date, hint) {
+  const dateString = date.toISOString().slice(0, 10);
+  return `---
+title: Wordle Hint for ${dateString}
+date: ${dateString}
+---
+
+# Wordle Hint for ${dateString}
+
+Here's a hint to help you solve today's Wordle: **${hint}**`;
+}
+
+// This is the main function that calls the other little async functions
+// It grabs the list of existing hints, finds the next date without a hint, and then calls the Wordle API to get the answer
+// Then it calls the ChatGPT API to write a hint, and it creates a new .mdx file with the hint
+exports.handler = async (event) => {
+  try {
+    const accessToken = process.env.GITHUB_ACCESS_TOKEN;
     const octokit = new Octokit({ auth: accessToken });
 
-    // Create the .mdx file in the GitHub repository
-    await octokit.repos.createOrUpdateFileContents({
+    const repoOwner = "imJarrad";
+    const repoName = "Just-a-Hint";
+
+    const existingHints = await getExistingHints(octokit, repoOwner, repoName);
+
+    const currentDate = new Date();
+    let nextDate = new Date(currentDate);
+    let foundDate = false;
+
+    while (!foundDate) {
+      nextDate.setDate(nextDate.getDate() + 1);
+      if (!existingHints.includes(`Wordle hint - ${nextDate.toISOString().slice(0, 10)}.mdx`)) {
+        foundDate = true;
+      }
+    }
+
+    const wordleResponse = await fetchWordle(nextDate);
+    if (wordleResponse.status === "ERROR") {
+      throw new Error("Wordle API returned an error: " + JSON.stringify(wordleResponse));
+    }
+    
+    const hint = await getHintFromChatGPT(wordleResponse.solution);
+     
+
+    const hintFileContent = createHintFileContent(nextDate, hint);
+    const base64Content = Buffer.from(hintFileContent).toString("base64");
+
+    const commitResponse = await octokit.repos.createOrUpdateFileContents({
       owner: repoOwner,
       repo: repoName,
-      path: `src/pages/blog/posts/${fileName}`,
-      message: `Add Wordle hint for ${formattedDate}`,
-      content: Buffer.from(fileContents).toString("base64"),
+      path: `src/pages/blog/posts/Wordle hint - ${nextDate.toLocaleString('default', { month: 'long' })} ${nextDate.getDate()}.mdx`,
+      message: `Add Wordle hint for ${nextDate.toISOString().slice(0, 10)}`,
+      content: base64Content,
       committer: {
-        name: repoOwner,
+        name: "imJarrad",
         email: "jarradmclean@gmail.com",
       },
       author: {
-        name: repoOwner,
+        name: "imJarrad",
         email: "jarradmclean@gmail.com",
       },
     });
 
     return {
       statusCode: 200,
-      body: "File created successfully",
+      body: JSON.stringify({
+        message: "Hint file created successfully",
+        hintDate: nextDate.toISOString().slice(0, 10),
+      }),
     };
   } catch (error) {
     console.error("Error creating file:", error);
     return {
       statusCode: 500,
-      body: "Error creating file",
+      body: JSON.stringify({ error: error.message }),
     };
   }
 };
-
-// This function calls the ChatGPT API with the Wordle answer and returns a hint
-async function getHintFromChatGPT(word) {
-    try {
-      const prompt = `Write a hint for the Wordle word: ${word}`;
-      const response = await openai.createCompletion({
-        model: "text-davinci-003",
-        prompt: prompt,
-        max_tokens: 50,
-        n: 1,
-        stop: null,
-        temperature: 0.7,
-      });
-  
-      if (response.data.choices && response.data.choices.length > 0) {
-        return response.data.choices[0].text.trim();
-      } else {
-        console.error("Error: No choices found in the API response:", response);
-        console.log("Choices array content:", response.data.choices); // Log the content of the choices array
-        throw new Error("No choices found in the API response");
-      }
-    } catch (error) {
-      console.error("Error calling OpenAI API:", error);
-      throw error;
-    }
-  }
-  
-  
